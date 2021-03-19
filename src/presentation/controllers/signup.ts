4cd87549ed5b48e5a3ca1caea74fddabc8d163aa
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-19 16:43:45 -0300

    feat: crete interfaces http

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, HttpResponse } from '../protocols'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
