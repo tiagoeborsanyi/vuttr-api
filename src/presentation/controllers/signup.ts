5c5a1a51e841de384616b676dca55459f52922be
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-19 16:38:54 -0300

    feat: create validation name and email existing

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,13 @@
 export class SignUpController {
-  handle (): boolean {
-    return true
+  handle (httpRequest: any): any {
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`missing param: ${field}`)
+        }
+      }
+    }
   }
 }
