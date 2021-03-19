ccf18419fe202d91b3be9c8560cf275b9ea9e9a6
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-19 16:39:19 -0300

    test: create validation name and email existing

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,31 @@
+import { SignUpController } from './signup'
+
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    expect(400).toBe(400)
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail',
+        password: 'any_pass',
+        passwordConfirmation: 'any_pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_pass',
+        passwordConfirmation: 'any_pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
   })
 })
