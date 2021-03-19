3993505c097ddf0c5e56d4987625404d61b9fae5
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-19 17:20:39 -0300

    feat: add errorParam on SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,13 @@
 import { HttpRequest, HttpResponse, Controller } from '../protocols'
+import { MissingParamError } from '../errors'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return {
-          statusCode: 400,
-          body: new Error(`missing param: ${field}`)
-        }
+        return badRequest(new MissingParamError(field))
       }
     }
   }
