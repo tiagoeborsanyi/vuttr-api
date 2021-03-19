089b212f1fa01bb64fb400af83041f5475a7ec1f
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-03-19 16:53:56 -0300

    feat: create interface for implement class SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,7 @@
-import { HttpRequest, HttpResponse } from '../protocols'
+import { HttpRequest, HttpResponse, Controller } from '../protocols'
 
-export class SignUpController {
-  handle (httpRequest: HttpRequest): HttpResponse {
+export class SignUpController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
